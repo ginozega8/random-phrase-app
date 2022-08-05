6400aa7b7617338a0b5612efc657d918eef7aeee
ginozega8 <ginozega8@outlook.es>
2022-08-05 15:11:59 -0300

    master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react"
+import { AiFillTwitterSquare } from "react-icons/ai";
 function App() {
+
+  const [quoteInfo, setQuoteInfo] = useState({})
+
+  useEffect(() => {
+    getQuote();     //Get Initial Quote
+  }, [])
+
+  const getQuote = () => {
+    fetch("https://api.quotable.io/random") //I used an api to get the quotes
+    .then((response) => {return response.json()})
+    .then((data) => {
+      setQuoteInfo({ // Assign text and author values
+        text: data.content,
+        author: data.author,
+      });
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="quote-box">
+        <p id="text">{quoteInfo.text}</p>
+        <p id="author">{quoteInfo.author}</p>
+
+        <button id="new-quote" onClick={getQuote}>New Quote</button>
+        <a href={'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + quoteInfo.text + " " + quoteInfo.author } id="tweet-quote" target="_blank" title="Tweet Quote"><AiFillTwitterSquare className="twitter-icon"/></a>
+
+      </div>
     </div>
   );
 }
